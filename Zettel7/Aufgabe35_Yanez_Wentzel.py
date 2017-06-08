0e85c837d0978629c9ce7f71abad8b9e6ff2fb87
Bianca Wentzel <courtney_boda@gmx.de>
2017-06-08 14:35:49 +0200

    Funktionsfähiges Backtracking, ohne Rückrichtung

--- Zettel7/Aufgabe35_Yanez_Wentzel.py
+++ Zettel7/Aufgabe35_Yanez_Wentzel.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Zettel 7, Informatik B
 # Elena Schmidt Yanez, Bianca Wentzel
 # Aufgabe 35
@@ -14,74 +15,64 @@ def munzeIstZulaessig(munze,betrag):
 def wechseln(muenztypen,betrag):
     muenzindex=0
     maxIndexMuenztypen=len(muenztypen)-1
-    teilloesung=[]
     loesung=[]
+    gesamtloesung=[]
+    lange = float("inf")
 
     while True:
         while True:
-            # wenn letztes Element der möglichen Münzstücke erreicht
+            # wenn aktuelle Münze die letzte in Liste
+            if muenzindex > maxIndexMuenztypen:
+                indexloesung=0
+                for index,teilloesung in enumerate(gesamtloesung):
+                    if len(teilloesung)<lange:
+                        lange=len(teilloesung)
+                        indexloesung=index
+                return gesamtloesung[indexloesung]
+
             if muenzindex==maxIndexMuenztypen:
-                # wenn aktuelles Münzstück zulässig
-                if munzeIstZulaessig(muenztypen[muenzindex]):
+                # wenn Münze zulässig
+                if munzeIstZulaessig(muenztypen[muenzindex], betrag):
+                    # wenn Lösung erreicht wäre
+                    if betrag - muenztypen[muenzindex]==0:
+                        betrag += sum(loesung)
+                        loesung.append(muenztypen[muenzindex])
+                        gesamtloesung.append(list(loesung))
+                        del loesung[:]
+                    else:
+                        loesung.append(muenztypen[muenzindex])
+                        betrag -= muenztypen[muenzindex]
+                        muenzindex = 0
+                # Münze ist nicht zulässig und letzte in Liste
+                else:
+                    break
 
 
 
+            if muenzindex < maxIndexMuenztypen:
+                # wenn gewählte Münze zulässig ist, dann schreibe diese in die Lösung
+                # und aktualisiere den Betrag, dann nimm wieder die erste Münze aus
+                # dem Münzstapel
+                if munzeIstZulaessig(muenztypen[muenzindex],betrag):
+                    if betrag - muenztypen[muenzindex]==0:
+                        betrag += sum(loesung)
+                        loesung.append(muenztypen[muenzindex])
+                        muenzindex = muenztypen.index(loesung[0]) + 1
+                        gesamtloesung.append(list(loesung))
+                        del loesung[:]
+                    else:
+                        loesung.append(muenztypen[muenzindex])
+                        betrag-=muenztypen[muenzindex]
+                        muenzindex=0
+                # Wenn die Münze nicht zulässig ist , nimm die nächste Münze
+                else:
+                    muenzindex+=1
 
 
-            if (munzeIstZulaessig(muenztypen[muenzindex], betrag) and muenzindex < maxIndexMuenztypen):
-                teilloesung.append(muenztypen[muenzindex])
-                betrag -= muenztypen[muenzindex]
-                muenzindex+=1
-
-            if (munzeIstZulaessig(muenztypen[muenzindex],betrag) and muenzindex==maxIndexMuenztypen):
-                if betrag-muenztypen[muenzindex]==0:
-                    betrag-=muenztypen[muenzindex]
-                    teilloesung.append(muenztypen[muenzindex])
-                    loesung.append(teilloesung)
-
-
-
-
-
-
-
-
-
-    # while True:
-    #     while True:
-    #         if (munzeIstZulaessig(muenztypen[muenzindex],betrag) and muenzindex < maxIndexMuenztypen):
-    #             loesung.append(muenztypen[muenzindex])
-    #             betrag-=muenztypen[muenzindex]
-    #             muenzindex+=1
-    #
-    #         elif (munzeIstZulaessig(muenztypen[muenzindex],betrag) and muenzindex==maxIndexMuenztypen):
-    #             if betrag == 0:
-    #                 return loesung
-    #             if betrag >0:
-    #                 betrag+=loesung[-1]
-    #                 loesung.pop()
-    #                 muenzindex-=1
-    #         else:
-    #             break
-    #
-    #
-    #     if betrag > 0:
-    #
-    #         if muenzindex == maxIndexMuenztypen:
-    #             betrag+=loesung[-1]
-    #             loesung.pop()
-    #             break
-    #
-    #         elif muenzindex < maxIndexMuenztypen:
-    #             muenzindex+=1
-    #
-    #         else:
-    #             muenzindex-=1
-    #             break
 
 # Ausprobieren der Funktion
-kleingeld=[50,10,5,2,20,1]
+kleingeld=[50,10,5,2,20,30,1]
 
-print(wechseln(kleingeld,33))
+print(wechseln(kleingeld,55))
 
 
